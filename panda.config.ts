d13cee83eb027ba927dd2aeba5d6f7ab11eb0a3c
Mike Murray <mikem.tba@gmail.com>
2023-09-22 01:55:32 +0200

    feat: ✨ Optimise panda for primitive use only

--- panda.config.ts
+++ panda.config.ts
@@ -1,10 +1,22 @@
-import { defineConfig } from "@pandacss/dev";
+import { defineConfig, type Tokens } from "@pandacss/dev";
+import { preset } from "@pandacss/preset-panda";
+
+const tokens = preset.theme.tokens;
+
+const newTokens: Partial<typeof preset.theme.tokens> = {
+  sizes: tokens.sizes,
+  spacing: tokens.spacing,
+  radii: tokens.radii,
+};
 
 export default defineConfig({
   strictTokens: false,
-  preflight: true,
-  presets: ["@pandacss/dev/presets"],
+  preflight: false,
+  // presets: ["@pandacss/dev/presets"],
   jsxFramework: "react",
   include: ["./src/**/*.{astro,tsx}"],
   outdir: "src/components/primitives",
+  clean: true,
+  theme: { tokens: newTokens as Tokens },
+  minify: true,
 });
