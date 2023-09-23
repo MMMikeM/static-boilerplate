6950ef7aaec1a777b29c1922dadc8c39a36ac192
Mike Murray <mikem.tba@gmail.com>
2023-09-23 04:59:15 +0200

    feat: ✨ Add header and nav

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,6 +1,7 @@
 import * as React from "react"
 import { type VariantProps, cva } from "class-variance-authority"
 import { cn } from "@components/ui/lib/utils"
+import { Slot } from "@radix-ui/react-slot"
 const buttonVariants = cva(
   "inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background",
   {
@@ -31,12 +32,15 @@ const buttonVariants = cva(
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof buttonVariants> {}
+    VariantProps<typeof buttonVariants> {
+  asChild?: boolean
+}
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, fullWidth, ...props }, ref) => {
+  ({ className, variant, size, fullWidth, asChild = false, ...props }, ref) => {
+    const Comp = props.asChild ? Slot : "button"
     return (
-      <button
+      <Comp
         className={cn(buttonVariants({ variant, size, fullWidth, className }))}
         ref={ref}
         {...props}
